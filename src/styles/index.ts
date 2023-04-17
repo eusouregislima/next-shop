f66cea7c756f04271cdbe4f857457729f2f297a5
eusouregislima <danilolima.cogic@gmail.com>
2023-04-17 08:31:31 -0300

    first commit

---
+++ src/styles/index.ts
@@ -0,0 +1,33 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} =
+  createStitches({
+    theme: {
+      colors: {
+        white: '#FFF',
+        black: '#000',
+
+        gray900: '#121214',
+        gray800: '#202024',
+        gray300: '#c4c4cc',
+        gray100: '#e1e1e6',
+
+        green500: '#00875f',
+        green300: '#00b37e',
+
+        purple300: '#BA55D3',
+        purple500: '#9932CC',
+        purple700: '#80007f'
+
+      }
+    }
+  })
